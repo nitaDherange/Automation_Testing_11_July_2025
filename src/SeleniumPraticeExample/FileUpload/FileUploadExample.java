412e435f23d3e3793a66f5a86abb677d8a2cb996
nitaDherange <nitadherange@gmail.com>
2025-07-14 17:28:07 +0530

    Update FileUploadExample.java

--- src/SeleniumPraticeExample/FileUpload/FileUploadExample.java
+++ src/SeleniumPraticeExample/FileUpload/FileUploadExample.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class FileUploadExample {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
